5a7346767e072c45dc5d9d111d215589ba3e9a53
Rishi <39877789+rishi-dev@users.noreply.github.com>
2020-03-30 16:06:08 +1100

    to cal
    to cal

--- WebContent/controller/propertybind.controller.js
+++ WebContent/controller/propertybind.controller.js
@@ -9,6 +9,46 @@ sap.ui.define([
             var bindingModel = this.getView().getModel("bindinglocal");
             //show local model
             console.log(this.getView().getModel("bindinglocal").getData().bindingModel.company);
-        }
+        },
+        onPressCAL:function(){
+                    console.log("trigger CAL");
+                   var  oDatamodel = this.getView().getModel('bindingcal');
+                    var mParameters = {           
+                        urlParameters: {
+                        },
+                        method: "GET",
+                        context: this,
+                        async: true,
+                        success: this.OrderTypeReadSuccessB.bind(this), 
+                        error: this.OrderTypeReadErrorB.bind(this)                        
+                    };       
+                    oDatamodel.read("/ProductSet('HT-9991')", mParameters); 
+
+        },
+
+        onPressES5:function(){
+            console.log("trigger ES5");
+            var oDatamodel = this.getView().getModel('bindingcal');
+            var mParameters = {
+                urlParameters: {
+                },
+                method: "GET",
+                context: this,
+                async: true,
+				success: this.OrderTypeReadSuccessB.bind(this), 
+				error: this.OrderTypeReadErrorB.bind(this)                
+            };       
+            oDatamodel.read("/ProductSet('HT-1000')", mParameters); 
+},
+
+		OrderTypeReadSuccessB: function(oData) {
+            console.log(oData);
+		},
+		OrderTypeReadErrorB: function(oError) {
+			console.log(oError);
+		}   
+
+
+            
     });
  });
