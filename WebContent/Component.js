c94fa29dd1dd96f079409dd01f98f901c7fbc562
Rishi <39877789+rishi-dev@users.noreply.github.com>
2020-04-01 15:28:22 +1100

    comchange1
    comchange1

--- WebContent/Component.js
+++ WebContent/Component.js
@@ -1,3 +1,5 @@
+//jQuery.sap.declare("sap.demo2020.Component");
+jQuery.sap.registerModulePath("sap.demo2020.Component", "/sap/bc/ui5_ui5/sap/zbinding_under");
 sap.ui.define([
     "sap/ui/core/UIComponent",
     "sap/ui/model/json/JSONModel"
